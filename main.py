33184616e74b2cf0547d5b52a19ed89206b78598
JenVega09 <JenVega09@gmail.com>
2023-03-17 16:56:00 -0500

    Completed the destination choice function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 print("Welcome to the Day Trip Generator!  With your input, this program will plan your perfect day trip... enjoy the experience!")
 
 destination = ["Jamaica","Costa Rica","San Francisco","San Antonio","Milwaukee"]
@@ -5,4 +7,15 @@ restaurants = ["Just Natural","Rasta Aide","Paloma","Cattleman's Steakhouse","La
 transportation = ["Uber", "Car", "Segway", "Bike", "Walking"]
 entertainment = ["Zipline","Waterpark","Hiking","Shopping","Snorkling"]
 
+def dest_options (destination):
+    i = True
+    while i == True:
+        dest_choice = input(f"How does {random.choice(destination)} sound for your destination?: y/n  ").lower()
+        if dest_choice == "n":
+            print ("Okay, let's try again.")
+        else:
+            print("Great, let's move on to the next feature of your trip!")       
+            i = False
+    return dest_choice
 
+final_dest = dest_options(destination)
