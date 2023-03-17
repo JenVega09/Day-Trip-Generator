fa463e92d76b28019f4f38f101915e25d9e75fe6
JenVega09 <JenVega09@gmail.com>
2023-03-17 11:44:08 -0500

    Added main.py and created Trip Generator Lists

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the Day Trip Generator!  With your input, this program will plan your perfect day trip... enjoy the experience!")
+
+destination = ["Jamaica","Costa Rica","San Francisco","San Antonio","Milwaukee"]
+restaurants = ["Just Natural","Rasta Aide","Paloma","Cattleman's Steakhouse","Lake Park Bistro"]
+transportation = ["Uber", "Car", "Segway", "Bike", "Walking"]
+entertainment = ["Zipline","Waterpark","Hiking","Shopping","Snorkling"]
+
+
